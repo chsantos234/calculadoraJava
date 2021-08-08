946ea4fa695a8c9f916387d5e81c26c8cec3a037
chsantos234 <77702696+chsantos234@users.noreply.github.com>
2021-08-08 12:41:09 -0300

    Update Calculadorajava.java

--- Calculadorajava.java
+++ Calculadorajava.java
@@ -1,8 +1,11 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+
 class CalculadoraPolinomio{
+	// Classe calculadora de polinômios que gerará o objeto calculadora2 para cálculos em cima da equação de segundo grau
 	public double a,b,c,delta,raiz1,raiz2;
 	
+	// métodos set e get para as variáveis da classe CalculadoraPolinomio:
 	void setA(double a) {this.a = a;}
 	double getA() {return this.a;}
 	
@@ -21,6 +24,7 @@ class CalculadoraPolinomio{
 	void setRaiz2(double raiz2) {this.raiz2 = raiz2;}
 	double getRaiz2() {return this.raiz2;}
 	
+	// métodos para os cálculos de delta e para as duas raízes:
 	void Delta(double a,double b,double c) {
 		double b2 = Math.pow(b, 2);
 		double resposta = b2 - 4*a*c;
@@ -40,25 +44,14 @@ class CalculadoraPolinomio{
 		double resposta = dividendo / (2 * a);
 		setRaiz2(resposta);
 	}
-	
-	void B0raiz1(double a,double c) {
-		double divisao = (c * -1) / a;
-		double resposta = Math.sqrt(divisao);
-		setRaiz1(resposta);
-	}
-	
-	void B0raiz2(double a,double c) {
-		double divisao = (c * -1) / a;
-		double raiz = Math.sqrt(divisao);
-		double resposta = raiz * -1;
-		setRaiz2(resposta);
-	}
 }
 
 
 public class Calculadorajava{
+	// Classe calculadora de polinômios que gerará o objeto calculadora1 para contas com operações binárias:
 	public double num1, num2, resultado;
 	
+	// métodos set e get para as variáveis da classe CalculadoraJava:
 	void setnum1(double numero) {this.num1 = numero;}
 	double getnum1() {return this.num1;}
 	
@@ -68,6 +61,7 @@ public class Calculadorajava{
 	void setResultado(double resultado) {this.resultado = resultado;}
 	double getResultado() {return this.resultado;}
 	
+	// métodos para os cálculos das diferentes operações binárias:
 	void soma(double num1,double num2) {
 		double resultado = num1 + num2;
 		setResultado(resultado);
@@ -85,7 +79,12 @@ public class Calculadorajava{
 	
 	void divisao(double num1,double num2) {
 		double resultado = num1 / num2;
-		setResultado(resultado);
+		if(num2 == 0) {
+			System.out.print("A resposta é indefinida");
+		}
+		else {
+			setResultado(resultado);
+		}
 	}
 	
 	void exponenciacao(double num1,double num2) {
@@ -98,6 +97,7 @@ public class Calculadorajava{
 		setResultado(resultado);
 	}
 	
+	// método main do programa:
 	public static void main(String args[]) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		Calculadorajava calculadora1 = new Calculadorajava();
@@ -110,8 +110,10 @@ public class Calculadorajava{
 			if(UserInput.length() > 1) {System.out.println("Comando inválido!");}
 			
 			else if(UserInput.equals("1")) {
+				// início da calculadora básica:
 				System.out.println("Calculadora básica...");
 				while(true) {
+					// início do loop da calculadora básica e recebimento de inputs:
 					System.out.println("Digite a operação à ser feita (+ , - , * , / , ** , % ou 0 para voltar)");
 					String UserOperation = br.readLine();
 					
@@ -131,6 +133,7 @@ public class Calculadorajava{
 					calculadora1.setnum2(Usernum2);
 					double num2 = calculadora1.getnum2();
 					
+					// chamada dos métodos para os diferentes cálculos e outputs finais;
 					if(UserOperation.equals("+")) {
 						calculadora1.soma(num1,num2);
 						double resultado = calculadora1.getResultado();
@@ -158,7 +161,7 @@ public class Calculadorajava{
 					else if(UserOperation.equals("**")) {
 						calculadora1.exponenciacao(num1,num2);
 						double resultado = calculadora1.getResultado();
-						System.out.printf("A exponenciação entre %.2f e %.2f é: %.2f%n",num1,num2,resultado);
+						System.out.printf("A exponenciação de %.2f elevado a %.2f é: %.2f%n",num1,num2,resultado);
 					}
 					
 					else if(UserOperation.equals("%")) {
@@ -168,11 +171,13 @@ public class Calculadorajava{
 					}
 					
 					else {System.out.printf("%s é um comando inválido.%n",UserOperation);}
-				}
+				}// fim do loop da calculadora básica
 			}
 			else if(UserInput.equals("2")) {
+				// início da calculadora de polinômios:
 				System.out.println("Calculadora de polinômio...");
 				while(true) {
+					// início do loop da calculadora de polinômios e recebimento de inputs:
 					System.out.println("Digite o valor de a ou s para voltar");
 					String UsernumaS = br.readLine();
 					if(UsernumaS.equals("s")) {break;}
@@ -201,28 +206,6 @@ public class Calculadorajava{
 					if(a == 0) {
 						System.out.println("A resposta é indefinida");
 					}
-					else if(b == 0) {
-						if(c > 0 ) {System.out.println("A resposta é indefinida");}
-						else {
-							calculadora2.B0raiz1(a,c);
-							calculadora2.B0raiz2(a,c);
-							
-							double raiz1 = calculadora2.getRaiz1();
-							double raiz2 = calculadora2.getRaiz2();
-							
-							System.out.printf("Resultado das raízes positivas e negativas: %.2f e %.2f.%n",raiz1,raiz2);
-						}
-					}
-					
-					else if(c == 0) {
-						calculadora2.Raiz1(delta, a, b);
-						calculadora2.Raiz2(delta, a, b);
-					
-						double raiz1 = calculadora2.getRaiz1();
-						double raiz2 = calculadora2.getRaiz2();
-						
-						System.out.printf("Resultado das duas raízes: %.2f e %.2f.%n",raiz1,raiz2);
-					}
 					
 					else if(delta < 0) {
 						System.out.printf("Resultado do delta:%.2f%n",delta);
@@ -237,15 +220,15 @@ public class Calculadorajava{
 						double raiz1 = calculadora2.getRaiz1();
 						double raiz2 = calculadora2.getRaiz2();
 					
-						System.out.printf("Resultado das raízes positivas e negativas: %.2f e %.2f.%n",raiz1,raiz2);
+						System.out.printf("Resultado das duas raízes: %.2f e %.2f.%n",raiz1,raiz2);
 					}
-				}
+				}// fim do loop da calculadora de polinômios
 			}
 			else if(UserInput.equals("0")) {
 				System.out.println("Até logo!");
 				break;
 			}
 			else {System.out.println("Comando inválido!");}
-		}
+		}// fim do loop principal
 	}
 }
